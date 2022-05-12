fa58f4123f95938e1bcf60544a350cba3b3d2a83
Younghwan Ji <younghwan.ji@gmail.com>
2022-05-12 23:23:34 +0900

    [setup] Activated ADMIN and Apply searching TestInfo model.
    1. How to make admin user?
     -> $ python manage.py createsuperuser
    2. How to access admin page?
     -> localhost:8000/admin
    3. Now you can search items based on user_id or image file name..

--- waikiki/ono/admin.py
+++ waikiki/ono/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TestInfo
+
+class TestInfoAdmin(admin.ModelAdmin):
+    search_fields = ['user_id', 'image']
+
+admin.site.register(TestInfo, TestInfoAdmin)
 
 # Register your models here.
